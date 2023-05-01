c415435b57ed56d0b8944d15a00d5d241fb06c68
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2023-05-01 12:23:14 +0900

    Comment Review image func and update func added

--- src/main/java/controllers/CommentReviewController.java
+++ src/main/java/controllers/CommentReviewController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,25 +11,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
 import commons.SecurityUtils;
 import dao.CommentReviewDAO;
+import dao.PhotoDAO;
 import dto.CommentReviewDTO;
+import dto.PhotoDTO;
 
 @WebServlet("*.commentReview")
 public class CommentReviewController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf8");
 		String cmd = request.getRequestURI();
+		Gson g = new Gson();
 		
 		try {
 			if(cmd.equals("/create.commentReview")) {
-				int score = Integer.parseInt(request.getParameter("score"));
-				String body = request.getParameter("body");
+				String realPath = request.getServletContext().getRealPath("CommentReview");
+				int maxSize = 1024 * 1024 * 10; //10Mb
+				System.out.println(realPath);
+				File realPathFile = new File(realPath);
+				if(!realPathFile.exists()) {
+					realPathFile.mkdir();
+				}
+				MultipartRequest multi = new MultipartRequest(request, realPath, maxSize, "utf8", new DefaultFileRenamePolicy());
+				
+				int score = Integer.parseInt(multi.getParameter("score"));
+				String body = multi.getParameter("body");
 				body = SecurityUtils.XSSCheck(body);
-				int storeID = Integer.parseInt(request.getParameter("storeID"));
+				int storeID = Integer.parseInt(multi.getParameter("storeID"));
 				int userNo = 0;			
 				int result = CommentReviewDAO.getInstance().insert(new CommentReviewDTO(0, body, score, storeID, userNo, null, 0));
+				int currval = CommentReviewDAO.getInstance().getCurrval();
 				
+				Enumeration<String> names = multi.getFileNames();
+				while(names.hasMoreElements()) {
+					String fileName = names.nextElement();
+					if(multi.getFile(fileName) != null){
+						String oriName = multi.getOriginalFileName(fileName);
+						String sysName = multi.getFilesystemName(fileName);
+						CommentReviewDAO.getInstance().insertPhoto(sysName, oriName, currval);
+					}
+				}
+				
+				response.sendRedirect("/view.store?storeID="+storeID);
+			}else if(cmd.equals("/getPhotoList.commentReview")) {
+				int reviewID = Integer.parseInt(request.getParameter("reviewID"));
+				ArrayList<PhotoDTO> imgList = CommentReviewDAO.getInstance().selectPhoto(reviewID);
+				
+				String resp = g.toJson(imgList);
+				
+				response.getWriter().append(resp);
+			}else if(cmd.equals("/photoDelete.commentReview")) {
+				int imageID = Integer.parseInt(request.getParameter("imageID"));
+				int storeID = Integer.parseInt(request.getParameter("storeID"));
+
+				int result = PhotoDAO.getInstance().delete(imageID);
+
 				response.sendRedirect("/view.store?storeID="+storeID);
 			}
 		}catch(Exception e) {
